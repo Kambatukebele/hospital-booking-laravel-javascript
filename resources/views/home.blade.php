36e37770059846eb997357059897e2a98a5baecb
KAMBA  TUKEBELE <91629041+Kambatukebele@users.noreply.github.com>
2023-11-09 08:00:54 +0100

    update 3 home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,14 @@
 <x-web-hospital>
-    <h1>Hello KAMBA</h1>
+    <!-- Image slider -->
+    <section class="w-full h-screen bg-slider-1 bg-center bg-no-repeat bg-cover flex justify-center items-center">
+        <div class="w-[90%] mx-auto lg:w-[950px] xl:w-[1200px] 2xl:w-[1400px] flex justify-center items-center flex-col">
+            <h1 class="text-2xl text-white font-bold text-center mb-5 md:text-4xl md:w-[400px] lg:text-7xl lg:w-[750px]">Best Treatment and Happy Life</h1>
+            <p class="text-base text-white font-normal text-center mb-5 md:w-[600px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, nesciunt voluptates! Obcaecati velit ab iusto?</p>
+            <div>
+                <button class="block w-36 text-white bg-blue-800 uppercase py-3 rounded-lg mb-2 hover:bg-blue-950 hover:transition-all hover:ease-in-out cursor-pointer sm:inline-block"><a href="#"></a>Check More</button>
+                <button class="block w-36 border text-white uppercase py-3 rounded-lg hover:bg-blue-950 hover:transition-all hover:ease-in-out cursor-pointer sm:inline-block"><a href="#">Find Doctors</a></button>
+            </div>
+        </div>
+    </section>
+    <!-- End Image slider -->
 </x-web-hospital>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ export default {
                 poppins : ['Poppins', 'sans-serif'],
             },
            backgroundImage: {
+            'slider-1' : "linear-gradient(90deg, rgba(21,17,89,0.8295693277310925) 0%, rgba(81,23,85,0.4066001400560224) 9%, rgba(11,109,165,0.5270483193277311) 53%, rgba(20,20,87,0.24413515406162467) 100%), url('/public/assets/images/slider-img-1.jpg')",
+            'slider-2' : "url('/public/assets/images/slider-img-2.jpg')",
+            'slider-3' : "url('/public/assets/images/slider-img-3.jpg')",
             'footer-bg-image' : "url('/public/assets/images/footer-bg1.png')"
            }
         },
